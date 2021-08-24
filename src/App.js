208a84c95fe161de83cbc3423b7eede28cf96e8d
#priyu@123 <priyaengg26@gmail.com>
2021-08-24 14:44:37 +0530

    deploy

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-
 import './App.css';
-import MoviesPages from './component/MoviesPages';
+import New from './component/New';
+import {BrowserRouter, Route , Switch} from 'react-router-dom'
+import MoviesPage from './component/MoviesPages';
+import NavBar from './component/NavBar';
 
 function App() {
   return (
-    <MoviesPages></MoviesPages>
+ 
+<BrowserRouter>
+<NavBar></NavBar>
+    <Switch>
+      
+      <Route to="/" exact component={MoviesPage}></Route>
+      <Route to="/new" exact component={New}></Route>
+    </Switch>
+    </BrowserRouter>
+
   );
 }
 
--- src/component/MoviesPages.jsx
+++ src/component/MoviesPages.jsx
@@ -1,100 +1,198 @@
 import React, { Component } from 'react'
-import { getMovies } from '../temp/movies'
-
-export default class MoviesPages extends Component {
+import { getMovies } from '../temp/movies';
+import List from "./List.jsx";
+import Pagination from "./Pagination.jsx";
+export default class MoviesPage extends Component {
     state = {
-        movies: getMovies(),
-        currSearchText:"",
-       // filteredMovies:getMovies()
+        movies: [],
+        genres: [{ id: 1, name: "All Genres" }],
+        currSearchText: "",
+        limit: 4,
+        currentPage: 1,
+        cGenre: "All Genres"
     }
-    deleteEntery = (id) => {
-        let filterMovies = this.state.movies.filter((movie) => {
-            return movie._id != id;
+    deleteEntry = (id) => {
+        let filtereMovies = this.state.movies.filter((movieObj) => {
+            return movieObj._id != id;
         })
         this.setState({
-            movies: filterMovies
+            movies: filtereMovies
         })
     }
-    handleInputValue = (e) => {
-        let currVal = e.target.value;
-      
-       this.setState({
-           currSearchText:currVal,
-           //filteredMovies:filteredArr
-       })
-    }
-    /* sortArray = (type) => {
-        const types = {
-          title: 'title',
-          stock: 'numberInStock',
-          rate: 'dailyRentalRate',
-        };
-        let sortProperty = types[type];
-        const sorted = this.state.filteredMovies.sort((a, b) => b.sortProperty > a.sortProperty);
-        console.log(sorted);
-       
-      };*/
-    render() {
-        let filteredArr = this.state.movies.filter((movie)=>{
-            let title = movie.title.trim().toLowerCase();
-            return title.includes(this.state.currSearchText)
+    setCurrentText = (e) => {
+        let task = e.target.value;
+        // filter 
+        this.setState({
+            currSearchText: task
         })
-        if(this.state.currSearchText == ""){
-            filteredArr = this.state.movies
+    }
+    sortByRatings = (e) => {
+        let className = e.target.className;
+        let sortedMovies;
+        let { movies } = this.state;
+        if (className == "fas fa-sort-up") {
+            sortedMovies = movies.sort((movieObjA, movieObjB) => {
+                return movieObjA.dailyRentalRate - movieObjB.dailyRentalRate;
+            });
+        } else {
+            sortedMovies = movies.sort((movieObjA, movieObjB) => {
+                return movieObjB.dailyRentalRate - movieObjA.dailyRentalRate;
+            });
         }
+        this.setState({
+            movies: sortedMovies
+        })
+    }
+    sortByStock = (e) => {
+        let className = e.target.className.trim();
+        let sortedMovies;
+        let { movies } = this.state;
+        if (className == "fas fa-sort-up") {
+            sortedMovies = movies.sort((movieObjA, movieObjB) => {
+                return movieObjA.numberInStock - movieObjB.numberInStock;
+            });
+        } else {
+            sortedMovies = movies.sort((movieObjA, movieObjB) => {
+                return movieObjB.numberInStock - movieObjA.numberInStock;
+            });
+        }
+        this.setState({
+            movies: sortedMovies
+        })
+    }
+    changelimit = (e) => {
+        // console.log("hello");
+        let currLimit = e.target.value;
+        if (currLimit < 1)
+            return;
+        // console.log(currPage);
+        this.setState({
+            limit: currLimit
+        })
+    }
+    changeCurrentPage = (pageNumber) => {
+        this.setState({
+            currentPage: pageNumber
+        })
+    }
+    groupBygenre = (name) => {
+        this.setState({
+            cGenre: name,
+            currSearchText: ""
+        })
+    }
+    async componentDidMount() {
+        // console.log(2);
+        let resp = await fetch("https://react-backend101.herokuapp.com/movies");
+        let jsonMovies = await resp.json();
+        this.setState({
+            movies: jsonMovies.movies
+        });
+        resp = await fetch("https://react-backend101.herokuapp.com/genres");
+        let jsonGenres = await resp.json();
+        this.setState({
+            genres: [...this.state.genres, ...jsonGenres.genres]
+        });
+    }
+    render() {
+        console.log(1);
+        let { movies, currSearchText, limit, currentPage, genres, cGenre } = this.state;
+        // console.log(movies);
+        //   genre
+        let filteredArr = movies;
+        if (cGenre != "All Genres") {
+            filteredArr = filteredArr.filter((movieObj) => {
+                return movieObj.genre.name == cGenre;
+            })
+        }
+        // search term 
+        if (currSearchText != "") {
+            filteredArr = filteredArr.filter((movieObj) => {
+                let title = movieObj.title.trim().toLowerCase();
+                // console.log(title);
+                return title.includes(currSearchText.toLowerCase());
+            })
+        }
+
+        // impliment
+        // console.log(filteredArr);
+        // si -> (pagenumber-1)*limit;
+        // eidx = si+limit;
+        // number of pages 
+        // paginate 
+        let numberofPage = Math.ceil(filteredArr.length / limit);
+        let si = (currentPage - 1) * limit;
+        let eidx = si + limit;
+        filteredArr = filteredArr.slice(si, eidx);
         return (
-            <div style={{ display: "flex" }} className="row">
-                <div className="movie-type col-3">
+            <div className="row" style={{padding:"10px"}}>
+                {/* 12 part */}
+                <div className="col-3">
+                    <List genres={genres} groupBygenre={this.groupBygenre}></List>
+                </div>
+                <div className="col-9">
+                    <input type="search" value={currSearchText}
+                        onChange={this.setCurrentText} />
+                    <input type="number" className="col-1"
+                        placeholder="no of elements/page"
+                        value={limit}
+                        onChange={this.changelimit}
+                    />
+                    {/* <input type="text" className="pageNumber"
+                        placeholder="page number" /> */}
                     <table className="table">
                         <thead>
                             <tr>
-                                <th scope="col">Title</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr>
-                                <th scope="col">acction</th>
-                            </tr>
-                            <tr>
-                                <th scope="col">thriller</th>
-                            </tr>
-                            <tr>
-                                <th scope="col">comedy</th>
-                            </tr>
-                        </tbody>
-                    </table></div>
-                <div className="table-container col-9">
-                    <input type="search" className="movie-detail" value={this.state.currSearchText} onChange={this.handleInputValue} />
-                    <table className="table" /*onClick={() => {this.sortArray("title")}}*/>
-                        <thead>
-                            <tr>
+                                <th scope="col">#</th>
                                 <th scope="col">Title</th>
                                 <th scope="col">Genre</th>
-                                <th scope="col">numberInStock</th>
-                                <th scope="col">Ratings</th>
-                                <th scope="col"></th>
+                                <th scope="col">
+                                    <i className="fas fa-sort-up" onClick={this.sortByStock}></i>
+                                    Stock
+                                    <i className="fas fa-sort-down" onClick={this.sortByStock}></i>
+                                </th>
+                                <th scope="col">
+                                    <i className="fas fa-sort-up" onClick={this.sortByRatings}></i>
+                                    Rate
+                                    <i className="fas fa-sort-down" onClick={this.sortByRatings}></i>
+                                </th>
                             </tr>
                         </thead>
                         <tbody>
+                            {filteredArr.map((movieObj) => {
+                                return (<tr scope="row" key={movieObj._id}>
+                                    <td> </td>
 
-                            {filteredArr.map((movie) => {
-                                return (
-                                    <tr key={movie._id}>
-                                        <th scope="col">{movie.title}</th>
-                                        <th scope="col">{movie.genre.name}</th>
-                                        <th scope="col">{movie.numberInStock}</th>
-                                        <th scope="col">{movie.dailyRentalRate}</th>
-                                        <th scope="col"><button type="button" onClick={() => { this.deleteEntery(movie._id) }} className="btn btn-danger">Delete</button></th>
-                                    </tr>
-
-                                )
+                                    <td>{movieObj.title} </td>
+                                    <td>{movieObj.genre.name}</td>
+                                    <td>{movieObj.numberInStock}</td>
+                                    <td>{movieObj.dailyRentalRate}</td>
+                                    <td><button type="button" className="btn btn-danger"
+                                        onClick={() => {
+                                            this.deleteEntry(movieObj._id);
+                                        }}>Delete</button></td>
+                                </tr>)
                             })}
-
                         </tbody>
                     </table>
-                </div>
+                    <Pagination
+                        numberofPage={numberofPage}
+                        currentPage={currentPage}
+                        changeCurrentPage={this.changeCurrentPage}
+                    ></Pagination>
 
-            </div>
+                </div>
+            </div >
+
         )
     }
 }
+
+// currentPage
+// this.changeCurrentPage(pageNumber)
+// /function
+// no of pages 
+// current page
+
+// genere array
+// /groupBygen
